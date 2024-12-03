71495065fd61e4c16ed3e7b72ea4947c9b4d4e43
Rushikesh Gherade <rushi422005@gmail.com>
2024-12-03 10:50:03 +0530

    Add files via upload

---
+++ lets_do_js/BASIC03/switch.js
@@ -0,0 +1,56 @@
+/*
+const chai = 3;
+
+switch(chai){
+    case 1: {
+        console.log("One");
+        break;
+    }
+    case 2:{
+        console.log("Two");
+        break;
+    }
+    case 3:{
+        console.log("Three");
+        break;
+    }
+    case 3:{
+        console.log("Three");
+        break;
+    }
+    case 4:{
+        console.log("Four");
+        break;
+    }
+    default:{
+        console.log("Enter valid");
+        break;
+    }
+         
+}
+*/
+/*
+let obj = {
+    name : "Rushi"
+}
+
+console.log(Object.keys(obj).length);
+
+if((Object.keys(obj)).length === 1){
+    console.log("Object is empty");
+}
+
+*/
+
+// console.log(true ==0 );
+/*
+//Nullish coalescing operator
+
+let val = 5 ?? 10;
+console.log(val);
+
+let val1 = null ?? 10;
+console.log(val1);
+let val2 =  undefined ?? 10;
+console.log(val2);
+*/
