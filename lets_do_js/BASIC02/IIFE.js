ab0cfdf9e380b784cdceb1e03e166415b6faa371
Rushi Gherade <rushi422005@gmail.com>
2024-12-03 10:47:30 +0530

    Your commit message

---
+++ lets_do_js/BASIC02/IIFE.js
@@ -0,0 +1,10 @@
+(function one(name){
+    // console.log(`${name} DB connected succeefully`);
+})("Rushi");
+
+( (name1) =>{
+    console.log(`${name1}`);
+})("Rushi");
+// (() => {
+//     console.log("Hey Rushi");
+// })();
--- lets_do_js/BASIC02/tempCodeRunnerFile.js
+++ lets_do_js/BASIC02/tempCodeRunnerFile.js
@@ -1,8 +1,3 @@
-={
-//     user:"Rushi",
-//     price:500,
-
-//     welcomeMessage : function(){
-//         console.log(`${this.user} Welcome`)
-//     }
-// }
+(() => {
+//     console.log("Hey Rushi");
+// })();
